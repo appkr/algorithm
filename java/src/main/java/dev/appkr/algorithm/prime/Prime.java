98ef7488a86baac4675b6abd904fedd0cc6cd175
appkr <juwonkim@me.com>
2021-09-13 14:35:32 +0900

    prime number

---
+++ java/src/main/java/dev/appkr/algorithm/prime/Prime.java
@@ -0,0 +1,24 @@
+package dev.appkr.algorithm.prime;
+
+public class Prime {
+
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      System.out.println(String.format("%d:%s", i, isPrime(i)));
+    }
+  }
+
+  private static boolean isPrime(int n) {
+    if (n < 2) {
+      return false;
+    }
+
+    for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
